21f415bea8ea62ffb263d9534bce7396222327b8
davschne <davschne@gmail.com>
2015-06-03 11:22:22 -0700

    Moved storage object to a separate file (js/storage.js); added <script> link to it in main.html

--- public/js/app.js
+++ public/js/app.js
@@ -66,29 +66,6 @@ function login() {
 
 $('startbutton').on('click', login);
 
-var storage = {
-  storeUser: function(user) {
-    var friends = user.getAllFriends();
-    localStorage.setItem(user.name, JSON.stringify(friends));
-  },
-
-  getUser: function(name) {
-    var user = new User(name);
-    var retrieved = localStorage.getItem(name);
-    if (retrieved) {
-      user.populateFriends(JSON.parse(retrieved));
-    }
-    return user;
-  }
-};
-
-// Restoring a user will look a bit like this:
-//
-// var name = $('input').value;
-// var user = storage.restoreUser(name);
-// friendPane.showPeople();
-// etc.
-
 function FriendPane(user) {
   $list = $('.friendsList');
   var friends = user.getAllFriends();
